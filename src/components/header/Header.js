6f3bb92bb87e8f53eee00f0b1338bbec343dfab3
Md.Shahriar Mridha Rahul <54900950+Rahul-1504022@users.noreply.github.com>
2022-06-24 21:50:31 +0600

    Navigation Bar added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navigation from "./Navigation";
 
 const Header = () => {
     return (
         <div>
-            <p>Header</p>
+            <Navigation />
         </div>
     );
 }
