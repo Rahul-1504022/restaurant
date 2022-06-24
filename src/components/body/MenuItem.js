a5491cf03e9aec6bb0643f1066481a9f60e56170
Md.Shahriar Mridha Rahul <54900950+Rahul-1504022@users.noreply.github.com>
2022-06-24 23:07:38 +0600

    Card Item design for Menu added

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Card, CardBody, CardHeader, CardImg, CardImgOverlay, CardSubtitle, CardTitle } from 'reactstrap';
 
 const MenuItem = (props) => {
-    console.log(props);
     return (
         <div>
+            <Card style={{ margin: "5px" }} >
+                <CardBody>
+                    <CardImg width="100%" alt={props.dish.name} src={props.dish.image} style={{ opacity: "0.5" }} />
+                    <CardImgOverlay>{props.dish.name}</CardImgOverlay>
+                </CardBody>
+            </Card>
 
-        </div>
+        </div >
     );
 }
 export default MenuItem;
